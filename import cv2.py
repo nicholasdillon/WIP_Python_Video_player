e6b3f447b4664a9854474087ffcd536cffd5f860
nicholasdillon <unclepeeweemc@gmail.com>
2023-10-14 13:53:33 -0500

    Update import cv2.py
    Updated the video player to be a bit more proficient

--- import cv2.py
+++ import cv2.py
@@ -2,74 +2,50 @@ import cv2
 import numpy as np
 
 def play_video(file_path):
-    # Open the video file
-    video = cv2.VideoCapture(file_path)
+    """Play the video at the given file path."""
 
-    # Check if the video file was opened successfully
+    video = cv2.VideoCapture(file_path)
     if not video.isOpened():
         print("Error opening video file")
         return
 
-    # Get the video's frame rate
     fps = video.get(cv2.CAP_PROP_FPS)
 
-    # Create a window to display the video
     cv2.namedWindow("Video Player", cv2.WINDOW_NORMAL)
-
-    # Set the window position
     cv2.moveWindow("Video Player", 100, 100)
-
-    # Set the window size
     window_width, window_height = 400, 300
     cv2.resizeWindow("Video Player", window_width, window_height)
 
-    # Initialize volume (start with maximum volume)
+    def handle_key_input(key):
+        """Handle key inputs and return whether to continue playing or not."""
+        if key == ord('q'):
+            return False
+        elif key == ord('m'):
+            nonlocal volume
+            volume = 1.0 if volume == 0 else 0
+        return True
+
     volume = 1.0
+    delay = int(1000 / fps)
 
     while True:
-        # Read the current frame from the video
         ret, frame = video.read()
-
         if not ret:
-            # End of the video
             break
 
-        # Convert frame color space to BGR
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-        # Convert frame to float32 for volume scaling
-        frame = frame.astype(np.float32)
-
-        # Set the volume (scale the frame by the volume)
-        frame = cv2.multiply(frame, volume)
-
-        # Convert frame back to uint8 for display
-        frame = cv2.convertScaleAbs(frame)
-
-        # Resize frame to fit the window size
+        # Here, we don't actually modify the video volume, we just simulate the effect by adjusting pixel brightness
+        frame = (frame * volume).astype(np.uint8)
         frame = cv2.resize(frame, (window_width, window_height))
 
-        # Display the frame in the window
         cv2.imshow("Video Player", frame)
 
-        # Wait for the specified amount of time (in milliseconds) based on the frame rate
-        delay = int(1000 / fps)
-        if cv2.waitKey(delay) == ord('q'):
-            # Press 'q' to quit the video player
+        key = cv2.waitKey(delay) & 0xFF  # wait for a key event
+        if not handle_key_input(key):
             break
-        elif cv2.waitKey(1) == ord('m'):
-            # Press 'm' to mute/unmute the video
-            if volume == 0:
-                # Unmute the video (restore the volume)
-                volume = 1.0
-            else:
-                # Mute the video (set volume to 0)
-                volume = 0
 
-    # Release the video object and close the window
     video.release()
     cv2.destroyAllWindows()
 
 # Usage example
-video_file = r ## Enter the path to the video file here ##
+video_file = r"your_path_here"  # Enter the path to the video file
 play_video(video_file)
